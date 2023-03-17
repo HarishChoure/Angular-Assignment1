38ba2a0591edfb5e275c74d690c7895223081718
Harish Choure <harishchoureoutlook@gmail.com>
2023-03-17 17:51:12 +0530

    EditDetails -updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ViewComponent } from './view/view.component';
+
+
+
+const appRout: Routes = [
+  
+  { path: 'Add-user', component: AddUserComponent },
+  { path : 'view-user', component:ViewComponent},
+  { path:'',component:ViewComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent,
+    ViewComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRout)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  
 })
 export class AppModule { }
